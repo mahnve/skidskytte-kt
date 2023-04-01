b3eeb483eb248bff77c60ec80d533f47726b00ed
Marcus Ahnve <m@hnve.org>
2023-04-01 23:29:52 +0200

    Fix tests

--- src/main/kotlin/se/marcusahnve/SkidskytteRoulette.kt
+++ src/main/kotlin/se/marcusahnve/SkidskytteRoulette.kt
@@ -15,17 +15,17 @@ import org.http4k.server.asServer
 import java.io.File
 
 
-val	prefixes = File(ClassLoader.getSystemResource("prefixes.txt").file).readLines()
-val	suffixes = File(ClassLoader.getSystemResource("suffixes.txt").file).readLines()
-
-val app: HttpHandler = routes(
+fun createApp(prefixes: List<String>, suffixes: List<String>): HttpHandler {
+    return routes(
     "/random" bind GET to {
         Response(OK).body(joinTwoRandomly(prefixes, suffixes))
-    }
-)
+    })
+}
 
 fun main() {
-    val printingApp: HttpHandler = PrintRequest().then(app)
+	val	prefixes = File(ClassLoader.getSystemResource("prefixes.txt").file).readLines()
+	val	suffixes = File(ClassLoader.getSystemResource("suffixes.txt").file).readLines()
+    val printingApp: HttpHandler = PrintRequest().then(createApp(prefixes, suffixes))
     val server = printingApp.asServer(Undertow(9000)).start()
     println("Server started on " + server.port())
 }
--- src/test/kotlin/se/marcusahnve/SkidskytteRouletteTest.kt
+++ src/test/kotlin/se/marcusahnve/SkidskytteRouletteTest.kt
@@ -1,29 +1,20 @@
 package se.marcusahnve
 
-import org.http4k.core.Method.GET
+import io.kotest.core.spec.style.ExpectSpec
+import org.http4k.core.Method
 import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Status
 import org.http4k.kotest.shouldHaveBody
 import org.http4k.kotest.shouldHaveStatus
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 
-class SkidskytteRouletteTest {
+class SkidskytteRouletteTest: ExpectSpec({
 
-    @Test
-    fun `Ping test`() {
-        assertEquals(app(Request(GET, "/ping")), Response(OK).body("pong"))
-    }
-    @Test
-    fun `Check Kotest matcher for http4k work as expected`() {
-        val request = Request(GET, "/testing/kotest?a=b").body("http4k is cool").header("my header", "a value")
-    
-        val response = app(request)
-    
-        // response assertions
-        response shouldHaveStatus OK
-        response shouldHaveBody "Echo 'http4k is cool'"
-    }
-
-}
+	context("an app with just one prefix and one suffix") {
+		val prefixes = listOf<String>("prefix")
+		val suffixes = listOf<String>("suffix")
+		val app = createApp(prefixes, suffixes)
+		val response = app(Request(Method.GET, "/random"))
+		response.shouldHaveStatus(Status.OK)
+		response.shouldHaveBody("prefixsuffix")
+	}
+})
